04f1c3ba38b3beb4485ab28bc638f79b95c940b2
Varshith Raj Basa <varshithraj.basa@gmail.com>
2023-10-09 20:43:38 +0530

    Added usage of Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./style.css";
 
+import Child from "Child";
+
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Child a="3" b="5" c="7" d="9">
+        <b>Child HTML of Component</b>
+      </Child>
     </div>
   );
 }
