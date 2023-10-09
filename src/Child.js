971fe8c56563eabf77bf95540b88c9f39bd24dec
Varshith Raj Basa <varshithraj.basa@gmail.com>
2023-10-09 21:01:37 +0530

    fix, issues and optimize

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,14 @@
 import React from "react";
 const Child = (props) => {
-    const { a, b, c } = props;
+    const { a, b } = props;
+    const values = props;
     const d = props.d;
+
     return (
         <div>
             <p>a:{a}</p>
             <p>b:{b}</p>
-            <p>c:{c}</p>
+            <p>c:{values.c}</p>
             <p>d:{d}</p>
             {props.children}
         </div>
